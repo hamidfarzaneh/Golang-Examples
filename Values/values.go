1c0f1299ab43651e3c15a5928cc068e01aba93e4
hamidfarzaneh <hamidfarzaneh75@gmail.com>
2019-05-27 03:45:32 +0430

    Values added

---
+++ Values/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// String concatination
+	fmt.Println("hamid " + "farzaneh")
+
+	// Mathematical operations
+	fmt.Println("1+2=", 1+2)
+
+	// Logical operations
+	fmt.Println(true && false)
+}
